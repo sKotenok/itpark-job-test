9bb3e9ba58470185c226ea7a56d779359016982d
sKotenok <chernbiy.kot@gmail.com>
2016-08-28 13:16:59 +0300

    Refactoring

--- 5.Refactoring/examples.py
+++ 5.Refactoring/examples.py
@@ -11,7 +11,12 @@ except:
     f.close()
 else:
     f.close()
+"""
+def myfunc():
+    with open('/tmp/data.txt') as f:
+        return f.readline()
 
+"""
 2.
 
 a = [1, 2, 3, 4]
@@ -20,7 +25,14 @@ for i in a:
     s+=i
 
 print s
+"""
+a = [1, 2, 3, 4]
+s = sum(a)
 
+print(s)
+
+
+"""
 3.
 
 a = [1, -2, 3, -4]
@@ -31,5 +43,12 @@ for i in a:
 
 print b
 """
+a = [1, -2, 3, -4]
 
+# v1.
+b = [i for i in a if i > 0]
+print(b)
 
+# v2.
+b = list(filter(lambda i: i > 0, a))
+print(b)
