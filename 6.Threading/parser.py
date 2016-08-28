8c3b96a4a87149865b86c35fdc37249ffa513a87
sKotenok <chernbiy.kot@gmail.com>
2016-08-28 21:53:35 +0300

    collections, threadings

--- 6.Threading/parser.py
+++ 6.Threading/parser.py
@@ -5,7 +5,7 @@
 
 Необходимо запустить количество thread-ов соответсвующее количеству файлов в директории.
 
-Пример файла файл
+Пример файла файл example.xml
 
 Каждый thread должен распарсить файл и сформировать объект следующей структуры:
 
@@ -24,5 +24,107 @@
 Ключи и значения объекта массива node (кромер node_id ) формируются из тега value, имеющиего атрибут genre="user".
 
 Каждый thread должен выполнить POST запрос на https://swfdev.com/ с передачей данного объекта.
+
 """
 
+#
+
+PATH = 'files' # '/opt/data'
+SERVER = 'http://127.0.0.1' # https://swfdev.com/
+
+
+import os, sys
+import json
+import threading
+import urllib
+from xml.etree import ElementTree
+
+
+if sys.version_info > (3, 0):
+    from urllib.parse import urlencode
+    from urllib.request import Request, urlopen
+
+    def get_xml_files(path):
+        # Python3 version with scandir
+        files = os.scandir(path)
+        return [f.path for f in files if f.is_file() and f.name.lower().endswith('.xml')]
+
+else:
+    from urllib import urlencode
+    from urllib2 import Request, urlopen
+
+    def get_xml_files(path):
+        # Python2 version
+        files = os.listdir(path)
+        result = []
+        for f_name in files:
+            f = os.path.join(path, f_name)
+            if os.path.isfile(f) and os.path.splitext(f)[-1].lower() == '.xml':
+                result.append(f)
+        return result
+
+
+def parse_gateway_nodes(xml_file, config):
+    xml_obj = ElementTree.parse(xml_file)
+    root = xml_obj.getroot()
+    if root.tag != 'poll':
+        return False
+    result = {
+        'gateway_id': root.get('gateway_id'),
+        'node': [],
+    }
+    for node in root.findall('node'):
+        node_dict = {}
+        for value in node:
+            node_dict['node_id'] = node.get('id')
+            if value.tag == 'value' and value.get('genre') == 'user':
+                node_dict[value.get('label')] = value.text
+        result['node'].append(node_dict)
+    return result
+
+
+def send_gateway_nodes(serializable_data, config):
+    uri = config['uri'] if 'uri' in config else 'http://127.0.0.1'
+    content_type = config['content-type'] if 'content-type' in config else 'application/json'
+
+    if content_type == 'application/json':
+        data_str = json.dumps(serializable_data).encode('utf8')
+    else:
+        data_str = urlencode(serializable_data)
+
+    req = Request(uri, data_str, headers={'content-type': content_type})
+    urlopen(req)
+
+
+
+class ParseThread(threading.Thread):
+    def __init__(self, xml_file, parser_func, sender_func, config):
+        super(ParseThread, self).__init__()
+        self.xml_file = xml_file
+        self.parser_func = parser_func
+        self.sender_func = sender_func
+        self.config = config
+
+    def run(self):
+        parsed_data = self.parser_func(self.xml_file, self.config)
+        self.sender_func(parsed_data, self.config)
+
+
+
+def main():
+    xml_files = get_xml_files(PATH)
+
+    threads = []
+    for f in xml_files:
+        t = ParseThread(f, parse_gateway_nodes, send_gateway_nodes, {'uri': SERVER, })
+        t.start()
+        threads.append(t)
+
+    # wait all threads to complete
+    for t in threads:
+        t.join()
+    print('All job is finished')
+
+
+if __name__ == '__main__':
+    main()
--- 8.Collections/nodes.py
+++ 8.Collections/nodes.py
@@ -45,10 +45,39 @@ id | node_id | node_name | type | name | min | max | title_label
 
 Продолжите/измените код:
 
-
 for row in query:
     node_id = row.node_id
     node_name = row.node_name
     type = row.type
 
 """
+
+class Row:
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            self.__dict__[key] = value
+
+query = [
+    Row(id=5, node_id=21, node_name='ZP3111-5 4-in-1', name='Luminance', type='sensor'),
+    Row(id=3, node_id=21, node_name='ZP3111-5 4-in-1', name='Relative Humidity', type='sensor'),
+    Row(id=1, node_id=23, node_name='ZP3111-5 4-in-1', name='Luminance', type='sensor'),
+]
+
+output = {}
+
+
+for row in query:
+    node_id = row.node_id
+
+    if node_id not in output:
+        output[node_id] = []
+
+    output[node_id].append({
+        'node_name': row.node_name,
+        'name': row.name,
+        'type': row.type
+    })
+
+
+from pprint import pprint
+pprint(output)
